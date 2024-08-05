057ff5175ae35066f86eaf425493be50d7b3405e
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2024-08-05 17:33:12 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const anime = require('./lib/anime');
+const manga = require('./lib/manga');
+const characters = require('./lib/characters');
+const people = require('./lib/people');
+const seasons = require('./lib/seasons');
+const search = require('./lib/search');
+const studios = require('./lib/studios');
+const reviews = require('./lib/reviews');
+const genres = require('./lib/genres');
+const user = require('./lib/user');
+
+module.exports = { 
+    anime, 
+    manga, 
+    characters, 
+    people, 
+    seasons, 
+    search, 
+    studios, 
+    reviews, 
+    genres, 
+    user 
+};
